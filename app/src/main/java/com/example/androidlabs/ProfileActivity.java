248f4c9d83f22e27c34b5b41157db3df657af784
anlinlily <an000031@algonquinlive.com>
2020-06-17 15:57:10 -0400

    Finished lab 3

---
+++ app/src/main/java/com/example/androidlabs/ProfileActivity.java
@@ -0,0 +1,92 @@
+package com.example.androidlabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    //cd /data/data/com.example.androidlabs/shared_prefs
+    //more sp.xml
+
+    private static final String ACTIVITY_NAME = "ProfileActivity";
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageButton pic;
+    private EditText getEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        Log.e(ACTIVITY_NAME, "In function: onCreate()");
+
+        getEmail = (EditText) findViewById(R.id.getEmail);
+        Intent fromMain = getIntent();
+        getEmail.setText(fromMain.getStringExtra("EMAIL"));
+
+        pic = (ImageButton) findViewById(R.id.pic);
+        pic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.e(ACTIVITY_NAME, "In function: onActivityResult()");
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            pic.setImageBitmap(imageBitmap);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(ACTIVITY_NAME, "In function: onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(ACTIVITY_NAME, "In function: onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(ACTIVITY_NAME, "In function: onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(ACTIVITY_NAME, "In function: onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(ACTIVITY_NAME, "In function: onDestroy()");
+    }
+}
